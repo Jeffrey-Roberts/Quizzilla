08ea2782c71a7461998fb231be30c9f4be2fbfed
Jeffrey Roberts <57999176+Jeffrey-Roberts@users.noreply.github.com>
2023-09-15 10:00:50 -0500

    added context/provider/reducer and QuizzilaCard type

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,26 @@
 import Constants from 'expo-constants';
 import { StatusBar } from 'expo-status-bar';
-import { useState } from 'react';
+import { useContext, useState } from 'react';
 import { FlatList, TextInput, StyleSheet, View, Keyboard } from 'react-native';
 
 import Card from './components/Card';
 import QuizzillaButton from './components/QuizzillaButton';
+import { QuizzillaContext } from './modules/QuizzillaContext';
 
 export default function App() {
-  const DATA = [
-    {
-      id: 1,
-      term: 'Term 1',
-      definition:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur euismod, turpis id tristique porttitor, odio nisl luctus libero, eu efficitur ex dolor eu velit. Duis vel tortor auctor, cursus diam a, maximus lectus. Praesent euismod, mi nec aliquet ornare, diam libero eleifend tortor, sed luctus neque arcu vel orci. Nam euismod, turpis a rutrum viverra, est dolor ultricies nunc, sed eleifend orci elit non enim. Duis id risus euismod, efficitur nunc et, tincidunt lectus. Proin et odio eu diam posuere aliquam. Nulla facilisi. Mauris auctor, nunc vitae fringilla faucibus, massa urna consectetur eros, eu placerat nunc ipsum in diam. Donec pellentesque, nibh sed efficitur pharetra, lectus nunc dapibus odio, ac posuere quam nunc nec massa. Proin euismod, elit a mollis tincidunt, enim nisl faucibus odio, nec consectetur sapien neque vitae lacus. Aliquam vitae elit auctor, pharetra nisl at, aliquet turpis. Morbi eget turpis auctor, pretium lorem a, vestibulum dui.',
-    },
-  ];
+  const { data, setData } = useContext(QuizzillaContext);
 
   const [termInputValue, setTermInputValue] = useState('');
   const [definitionInputValue, setDefinitionInputValue] = useState('');
-  const [data, setData] = useState(DATA);
 
   const handleAdd = () => {
     if (termInputValue && definitionInputValue) {
-      const newData = [
-        ...data,
-        {
-          id: data.length + 1,
-          term: termInputValue,
-          definition: definitionInputValue,
-        },
-      ];
-      setData(newData);
+      const newData = {
+        id: data.length + 1,
+        term: termInputValue,
+        definition: definitionInputValue,
+      };
+      setData({ type: 'ADD_CARD', payload: newData });
       setTermInputValue('');
       setDefinitionInputValue('');
       Keyboard.dismiss();
