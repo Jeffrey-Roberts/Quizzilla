c8c7a4e351bae1657bd64f3ede04f1502bbfaf54
Jeffrey-Roberts <jeffreyjayroberts@gmail.com>
2024-02-19 09:38:19 -0600

    added jest-mock-axios, added postTerm endpoint, updated tests

--- __test__/App.spec.tsx
+++ __test__/App.spec.tsx
@@ -1,122 +1,146 @@
-import { act, render, screen, userEvent } from '@testing-library/react-native';
+import { render, act, screen, userEvent } from '@testing-library/react-native';
+import { UserEventInstance } from '@testing-library/react-native/build/user-event/setup';
+import axios from 'axios';
 import React from 'react';
 
 import App from '../src/App';
 import { QuizzillaCProvider } from '../src/modules/QuizzillaContext';
 
+jest.mock('axios');
+
 describe('App', () => {
+  let mockPost: jest.MockedFunction<typeof axios.post>;
+  let user: ReturnType<typeof userEvent.setup>;
+  let termInput: HTMLElement;
+  let definitionInput: HTMLElement;
+  let submitButton: HTMLElement;
+
+  beforeEach(() => {
+    mockPost = axios.post as jest.MockedFunction<typeof axios.post>;
+    mockPost.mockResolvedValue({
+      data: { id: 1, term: 'Test Term', definition: 'Test Definition' },
+    });
+
+    const { getByLabelText } = render(
+      <QuizzillaCProvider>
+        <App />
+      </QuizzillaCProvider>
+    );
+    user = userEvent.setup();
+    termInput = getByLabelText('term input');
+    definitionInput = getByLabelText('definition input');
+    submitButton = getByLabelText('submit button');
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
   test('renders the app', () => {
     render(<App />);
     expect(screen.getByLabelText('root')).toBeTruthy();
   });
 
-  test('given term and definition when user submits form then card is added', async () => {
-    render(
-      <QuizzillaCProvider>
-        <App />
-      </QuizzillaCProvider>
-    );
-    const user = userEvent.setup();
-    const termInput = screen.getByLabelText('term input');
-    const definitionInput = screen.getByLabelText('definition input');
-    const submitButton = screen.getByLabelText('submit button');
+  test('given valid term and definition when user submits form then axios post request is sent and context is updated', async () => {
     const term = 'Test Term';
     const definition = 'Test Definition';
 
-    await act(async () => {
-      await user.type(termInput, term);
-      await user.type(definitionInput, definition);
-      await user.press(submitButton);
+    await submitForm(
+      user,
+      termInput,
+      definitionInput,
+      submitButton,
+      term,
+      definition
+    );
 
-      expect(screen.getByLabelText('card-1')).toBeTruthy();
+    await act(async () => {
+      expect(mockPost).toHaveBeenCalledWith('http://localhost:8080/term', {
+        id: 1,
+        name: term,
+        description: definition,
+      });
     });
+    expect(screen.getByLabelText('card-1')).toBeTruthy();
   });
 
   test('given no term with definition when user submits form then no card is added', async () => {
-    render(
-      <QuizzillaCProvider>
-        <App />
-      </QuizzillaCProvider>
-    );
-    const user = userEvent.setup();
-    const termInput = screen.getByLabelText('term input');
-    const definitionInput = screen.getByLabelText('definition input');
-    const submitButton = screen.getByLabelText('submit button');
     const term = '';
     const definition = 'Test Definition';
 
-    await act(async () => {
-      await user.type(termInput, term);
-      await user.type(definitionInput, definition);
-      await user.press(submitButton);
+    await submitForm(
+      user,
+      termInput,
+      definitionInput,
+      submitButton,
+      term,
+      definition
+    );
 
-      expect(screen.queryByLabelText('card-1')).toBeFalsy();
+    await act(async () => {
+      expect(mockPost).not.toHaveBeenCalledWith('http://localhost:8080/term', {
+        id: 1,
+        name: term,
+        description: definition,
+      });
     });
+    expect(screen.queryByLabelText('card-1')).toBeFalsy();
   });
 
   test('given term with no definition when user submits form then no card is added', async () => {
-    render(
-      <QuizzillaCProvider>
-        <App />
-      </QuizzillaCProvider>
-    );
-    const user = userEvent.setup();
-    const termInput = screen.getByLabelText('term input');
-    const definitionInput = screen.getByLabelText('definition input');
-    const submitButton = screen.getByLabelText('submit button');
     const term = 'Test Term';
     const definition = '';
 
-    await act(async () => {
-      await user.type(termInput, term);
-      await user.type(definitionInput, definition);
-      await user.press(submitButton);
+    await submitForm(
+      user,
+      termInput,
+      definitionInput,
+      submitButton,
+      term,
+      definition
+    );
 
-      expect(screen.queryByLabelText('card-1')).toBeFalsy();
+    await act(async () => {
+      expect(mockPost).not.toHaveBeenCalledWith('http://localhost:8080/term', {
+        id: 1,
+        name: term,
+        description: definition,
+      });
     });
+    expect(screen.queryByLabelText('card-1')).toBeFalsy();
   });
 
   test('when user deletes card then card is removed', async () => {
-    render(
-      <QuizzillaCProvider>
-        <App />
-      </QuizzillaCProvider>
-    );
-    const user = userEvent.setup();
-    const termInput = screen.getByLabelText('term input');
-    const definitionInput = screen.getByLabelText('definition input');
-    const submitButton = screen.getByLabelText('submit button');
     const term = 'Test Term';
     const definition = 'Test Definition';
 
-    await act(async () => {
-      await user.type(termInput, term);
-      await user.type(definitionInput, definition);
-      await user.press(submitButton);
+    await submitForm(
+      user,
+      termInput,
+      definitionInput,
+      submitButton,
+      term,
+      definition
+    );
 
-      await user.press(screen.getByLabelText('delete card 1'));
-      expect(screen.queryByLabelText('card-1')).toBeFalsy();
-    });
+    await user.press(screen.getByLabelText('delete card 1'));
+    expect(screen.queryByLabelText('card-1')).toBeFalsy();
   });
 
   test('when user edits card then card is updated', async () => {
-    render(
-      <QuizzillaCProvider>
-        <App />
-      </QuizzillaCProvider>
-    );
-    const user = userEvent.setup();
-    const termInput = screen.getByLabelText('term input');
-    const definitionInput = screen.getByLabelText('definition input');
-    const submitButton = screen.getByLabelText('submit button');
     const term = 'Test Term';
     const definition = 'Test Definition';
 
-    await act(async () => {
-      await user.type(termInput, term);
-      await user.type(definitionInput, definition);
-      await user.press(submitButton);
+    await submitForm(
+      user,
+      termInput,
+      definitionInput,
+      submitButton,
+      term,
+      definition
+    );
 
+    await act(async () => {
       await user.press(screen.getByLabelText('edit card 1'));
       const editTermInput = screen.getByLabelText('edit term input');
       const editDefinitionInput = screen.getByLabelText(
@@ -138,3 +162,18 @@ describe('App', () => {
     });
   });
 });
+
+async function submitForm(
+  user: UserEventInstance,
+  termInput: HTMLElement,
+  definitionInput: HTMLElement,
+  submitButton: HTMLElement,
+  term: string,
+  definition: string
+) {
+  await act(async () => {
+    await user.type(termInput, term);
+    await user.type(definitionInput, definition);
+    await user.press(submitButton);
+  });
+}
